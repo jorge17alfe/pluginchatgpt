4fdf6be84d1a285232702289ec8c0dbc18e737a2
jorge17alfe <jorgeordonezmorales@gmail.com>
2024-02-27 16:02:38 +0100

    update 27/02/24 (2)

--- app/controller/gptController.php
+++ app/controller/gptController.php
@@ -1,5 +1,7 @@
 <?php
-include_once __DIR__ . "/../../vendor/autoload.php";
+if (file_exists(__DIR__ . "/../../vendor/autoload.php")) {
+    include_once __DIR__ . "/../../vendor/autoload.php";
+}
 
 class GptController
 {
@@ -101,16 +103,16 @@ class GptController
         // return "Redactame un articulo completo de {$title} para una pagina web, con titulo principal envuelto en h1  y subtitulos en al menos 10 secciones envueltos en h3 un link a con src ";
         // return "un poema hacia  {$title} ";
         return
-          [  
-            "Vamos a redactar un articulo en español paso a paso para una web de {$title} con diferentes secciones del articulo principal de {$title}.\n Para empezar: dame un titulo envuelto en etiqueta h1",
-            "   Crea un texto de  pagina web sin titulo principal que ya lo edito yo:
+            [
+                "Vamos a redactar un articulo en español paso a paso para una web de {$title} con diferentes secciones del articulo principal de {$title}.\n Para empezar: dame un titulo envuelto en etiqueta h1",
+                "   Crea un texto de  pagina web sin titulo principal que ya lo edito yo:
             Un subtitulo con una descripcion de entre 2 y 3 líneas envuelto en h3. 
             Haz un top 10 de {$title} enumerados envueltos en <b>, con una descripcion de al menos 3 líneas. 
             una seccion con titulo envuelto en h3  hablando de porque estos articulos que comprarán en tu tienda son los mejores y deben de comprarlos.
             A continuacion elabora una seccion con un subtitulo con cada elemento del top 10 y un texto de 150 a 180 palabras hablado de su historia.
             Y una sección de parrafo envuelto en negrita  hablando de los buenos resultados que darian.
             "
-        ];
+            ];
     }
 
     public function ResponseAll()
